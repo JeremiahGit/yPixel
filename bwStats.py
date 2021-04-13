599f6a78aa009a3acdfe4068b63a857cab7b3722
Youngthanael <goza.jag@gmail.com>
2021-04-13 08:57:06 -0500

    added more things to bwStats.py Wins and losses

--- bwStats.py
+++ bwStats.py
@@ -71,14 +71,68 @@ def getFinalDeaths(pData, index):
 #      updated to match
 #
 def getFKDR(pData, index):
-        return float(getFinalKills(pData, index)) / getFinalDeaths(pData,index)
+    return float(getFinalKills(pData, index)) / getFinalDeaths(pData,index)
 
+#
+# Returns wins of a player
+# @parameter pData = player data .json file
+# @parameter index = What Bedwars mode to get number of wins from
+#                    0 Overall, 1 Solos, 2 Doubles, 3 Threes, 4 Fours, 5 4v4, 6 Dreams
+# TODO overall dream wins
+#
+def getWins(pData, index):
+    if(pData["success"]):
+        FINAL_KILLS_DICT = {
+            0: pData["player"]["stats"]["Bedwars"]["wins_bedwars"],
+            1: pData["player"]["stats"]["Bedwars"]["eight_one_wins_bedwars"],
+            2: pData["player"]["stats"]["Bedwars"]["eight_two_wins_bedwars"],
+            3: pData["player"]["stats"]["Bedwars"]["four_three_wins_bedwars"],
+            4: pData["player"]["stats"]["Bedwars"]["four_four_wins_bedwars"],
+            5: pData["player"]["stats"]["Bedwars"]["two_four_wins_bedwars"],
+            6: pData["player"]["stats"]["Bedwars"]["wins_bedwars"] #TODO
+        }
+        return FINAL_KILLS_DICT.get(index, "ERROR: Invalid index. Use a number in the inclusive range 0-6")
+    elif (index > 6):
+        print("ERROR: Invalid index. Use a number in the inclusive range 0-6")
+        return -1
+    else:
+        print("ERROR: Invalid Player Data, please input valid player data.")  
+        return -1
 
-def getWins(pdata, index):
-    print("hi")
+#
+# Returns losses of a player
+# @parameter pData = player data .json file
+# @parameter index = What Bedwars mode to get number of losses from
+#                    0 Overall, 1 Solos, 2 Doubles, 3 Threes, 4 Fours, 5 4v4, 6 Dreams
+# TODO overall dream losses
+#
+def getLosses(pData, index):
+    if(pData["success"]):
+        FINAL_KILLS_DICT = {
+            0: pData["player"]["stats"]["Bedwars"]["losses_bedwars"],
+            1: pData["player"]["stats"]["Bedwars"]["eight_one_losses_bedwars"],
+            2: pData["player"]["stats"]["Bedwars"]["eight_two_losses_bedwars"],
+            3: pData["player"]["stats"]["Bedwars"]["four_three_losses_bedwars"],
+            4: pData["player"]["stats"]["Bedwars"]["four_four_losses_bedwars"],
+            5: pData["player"]["stats"]["Bedwars"]["two_four_losses_bedwars"],
+            6: pData["player"]["stats"]["Bedwars"]["losses_bedwars"] #TODO
+        }
+        return FINAL_KILLS_DICT.get(index, "ERROR: Invalid index. Use a number in the inclusive range 0-6")
+    elif (index > 6):
+        print("ERROR: Invalid index. Use a number in the inclusive range 0-6")
+        return -1
+    else:
+        print("ERROR: Invalid Player Data, please input valid player data.")  
+        return -1
 
-def getLosses():
-    print(-1)
-
-def getWLRatio():
-    print(-1)
+#
+# Returns the win loss ratio aka FKDR
+# @parameter pData = player data .json file
+# @parameter index = What Bedwars mode to get number of final kills from
+#                    0 Overall, 1 Solos, 2 Doubles, 3 Threes, 4 Fours, 5 4v4, 6 Dreams
+# TODO Manually remoce the 4v4 wins and losses from the calculated overall FKDR.
+#      It seems that in the update where 4v4 was removed from stats, the API was not 
+#      updated to match
+#
+def getWLRatio(pData, index):
+    return float(getWins(pData, index)) / getLosses(pData,index)
